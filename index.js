bffdd467f8db730c7be163ec7bf4f331800a607b
LeaCrazyBambi <lheynitz@gmail.com>
2021-05-02 11:40:12 +0200

    Juhuuu, I made it :D

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if (a + b < 4) {
 let a=3;
 let b=6;
 
-//write your code here 
+const result = (a + b < 4) ? 'Below' : 'Over';
 console.log(result)
 
 /* _________________________________ */
@@ -32,9 +32,7 @@ else
 /* _________________________________ */
 
 let score = 42;
-let msg = "";
-
-//write your code here 
+let msg = (score > 1337) ? "This is a new highscore!" : "You need more points to beat the highscore!";
 
 console.log(msg)
 
@@ -57,9 +55,10 @@ if (login == 'Employee') {
 } */
 
 /* _________________________________ */
+let login = '';
+const message = (login == 'Employee') ? 'Hello' : (login == 'Director') ? 'Greetings' : (login == '') ? 'No login' : '';
+ 
 
-
-//write your code here 
 console.log(message)
 
 /* _________________________________ */
